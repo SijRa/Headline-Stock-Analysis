6df024f8dee9949167698a2b1b551b57eddaff86
Sijan Shreesh Rana <Sijan_Rana@hotmail.com>
2019-06-17 17:46:22 +0100

    Delete open_close_data_prep.py

--- open_close_data_prep.py
+++
@@ -1,9 +0,0 @@
-from stock_data_parser import Stock_Data_Object
-
-
-
-
-# Create stock object
-stockObject = Stock_Data_Object('fb-stock-history')
-# Get stock history dates
-article_dates = stockObject.Get_DateColumn()
